c2062972557a6564f5d2452f57b544e212977730
Jhaycee Llaneta <43661903+johncarlosllaneta@users.noreply.github.com>
2020-10-12 15:14:57 +0800

    content

--- jsp_English/lessons-english.js
+++ jsp_English/lessons-english.js
@@ -1,25 +1,27 @@
-
+//lesson content
 let container = document.createElement('div');
 container.className = "container-eng";
 container.setAttribute("onclick","showIDLesson(1)");
 
+//div > row
 let container_row = document.createElement('div');
 container_row.className = "row";
 
-
-
+//div > column_img
 let container_column_img = document.createElement('div');
 container_column_img.className = "column-img";
 
+//content of column_img
 let imgsrc = document.createElement("img");
 imgsrc.className = "image";
 imgsrc.setAttribute("src","");
 container_column_img.appendChild(imgsrc);
 
-
+//div  > column_desc
 let container_column_desc = document.createElement('div');
 container_column_desc.className = "column-desc";
 
+//content of column_desc
 let title = document.createElement("h1");
 var titletext = document.createTextNode("Phonetics");
 title.appendChild(titletext);
@@ -30,12 +32,14 @@ let hrline= document.createElement("hr");
 let textcontent = document.createElement("h3");
 var content = document.createTextNode("Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis consectetur libero accusamus eum molestias qui accusantium corrupti autem consequatur und");
 textcontent.appendChild(content);
+//
 
+//appendingChild to container_column_desc
 container_column_desc.appendChild(title);
 container_column_desc.appendChild(hrline);
 container_column_desc.appendChild(textcontent);
 
-
+//appendingChild to container and container_row
 container.appendChild(container_row);
 container_row.appendChild(container_column_img);
 container_row.appendChild(container_column_desc);
