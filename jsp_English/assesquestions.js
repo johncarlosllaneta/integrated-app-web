1cc87ef93d42da55dd71f0ba2840c2b301179426
Jhaycee Llaneta <43661903+johncarlosllaneta@users.noreply.github.com>
2020-11-10 18:51:40 +0800

    img
    imgs

--- jsp_English/assesquestions.js
+++ jsp_English/assesquestions.js
@@ -2,18 +2,53 @@
 
 const english1 = [
 {
-    q: 'When is ur birthday?',
-    options:['January','February','May','June'],
-    answer:2
-},
-{
-    q: 'Who is ur bias?',
-    options:['Sana','Mina','Chaeng','Dahyun'],
+    q: 'What is your response if your friend ask you how are you?',
+    options:['I\'m doing great, how about you?','I\'m shy.','I\'m sad.'],
     answer:0
 },
 {
-    q: 'Is ur favorite music streaming service is Spotify?',
-    options:['True','False'],
+    q: 'What is your response if your friend told you Good Morning?',
+    options:['Good Morning Too.','Great Job','Thank you.'],
+    answer:0
+},
+{
+    q: 'What will you say if you\'ve done something wrong?',
+    options:['Sorry.','Excuse Me.','Thank you.'],
+    answer:0
+},
+{
+    q: 'What is your response if your friend told you Good bye , see you?',
+    options:['Goodbye, see you later too.','Sorry.','I\'m doing great, how about you?'],
+    answer:0
+},
+{
+    q: 'What will you say if your friend help you?',
+    options:['Thank you','Sorry','Goodbye, see you later too'],
+    answer:0
+},
+{
+    q: 'What is your response if your friend told you Thank you?',
+    options:['You\'re welcome.','Sorry.','I\'m doing great, how about you?'],
+    answer:0
+},
+{
+    q: 'What will you say to your teacher if you need to go to the bathroom?',
+    options:['Teacher may I go out? I need to go to the bathroom.','Teacher may I go out? I need to go to the canteen.','Teacher may I go out? I need to go to the library.'],
+    answer:0
+},
+{
+    q: 'What will you say if you\'re making too much noise?',
+    options:['Excuse Me','Thank you','Goodbye, see you later too'],
+    answer:0
+},
+{
+    q: 'What will you say if your friend had sick?',
+    options:['How are you?','You\'re welcome.','Goodbye, see you later too'],
+    answer:0
+},
+{
+    q: 'What will you say if you need something?',
+    options:['Please','How are you?','You\'re welcome.'],
     answer:0
 }
 ]
@@ -21,8 +56,28 @@ const english1 = [
 
 const english2 = [
     {
-        q: 'When is ur birthday?',
-        options:['January','February','May','June'],
+        q: 'Type the missing vowels of the image <br> <img src="/img_English/ant.jpg" alt="Girl in a jacket" width="100%,mkjhz" height="10%">',
+        options:['A','E','I','O','U'],
+        answer:0
+    },
+    {
+        q: 'Type the missing vowels of the image <br> <img src="/img_English/bug.jpg" alt="Girl in a jacket" width="100%,mkjhz" height="10%">',
+        options:['A','E','I','O','U'],
+        answer:4
+    },
+    {
+        q: 'Type the missing vowels of the image <br> <img src="/img_English/egg.jpg" alt="Girl in a jacket" width="100%,mkjhz" height="10%">',
+        options:['A','E','I','O','U'],
+        answer:1
+    },
+    {
+        q: 'Type the missing vowels of the image <br> <img src="/img_English/fox.jpg" alt="Girl in a jacket" width="100%,mkjhz" height="10%">',
+        options:['A','E','I','O','U'],
+        answer:3
+    },
+    {
+        q: 'Type the missing vowels of the image <br> <img src="/img_English/fish.jpg" alt="Girl in a jacket" width="100%,mkjhz" height="10%">',
+        options:['A','E','I','O','U'],
         answer:2
     },
     {
@@ -59,20 +114,52 @@ const english3 = [
     
 const english4 = [
     {
-        q: 'When is ur birthday?',
-        options:['January','February','May','June'],
+        q: 'Find the noun of each sentences : The boy is playing.',
+        options:['The','Boy','Playing','none'],
+        answer:1
+    },
+    {
+        q: 'Find the noun of each sentences : The doctor is working.',
+        options:['The','Doctor','Working','none'],
+        answer:1
+    },
+    {
+        q: 'Find the noun of each sentences : The Dog is barking.',
+        options:['The','Dog','Barking','none'],
+        answer:1
+    },
+    {
+        q: 'Find the noun of each sentences : The fireman rushes towards the burning houses.',
+        options:['Fireman','Rushes','Houses','none'],
+        answer:0
+    },
+    {
+        q: 'Find the noun of each sentences : There are many trees on the garden.',
+        options:['Trees','Garden','Both A and b','none'],
         answer:2
     },
     {
-        q: 'Who is ur bias?',
-        options:['Sana','Mina','Chaeng','Dahyun'],
+        q: 'Fill in the blank : _____ is my father. ',
+        options:['She','He','I','We'],
+        answer:1
+    },
+    {
+        q: 'Fill in the blank : _____ is my mother. ',
+        options:['She','He','I','We'],
         answer:0
     },
     {
-        q: 'Is ur favorite music streaming service is Spotify?',
-        options:['True','False'],
-        answer:0
+        q: 'Fill in the blank : ____ is my pet. ',
+        options:['She','He','It','We'],
+        answer:2
+    },
+    {
+        q: 'Fill in the blank : _____ are family. ',
+        options:['She','He','It','We'],
+        answer:3
     }
+    
+    
     ]
 
     
@@ -114,54 +201,114 @@ const english6 = [
     
 const english7 = [
     {
-        q: 'When is ur birthday?',
-        options:['January','February','May','June'],
+        q: ' In the story Jack and the Beanstalk, what is the stranger exchanging for the cow of Jack?',
+        options:['Magic Beans','Century Beans','Holy Beans'],
+        answer:0
+    },
+    {
+        q: 'In the story of Five W’s, how many dog does Jody have?',
+        options:['1 dogs','2 dogs','3 dogs','7 dogs'],
         answer:2
     },
     {
-        q: 'Who is ur bias?',
-        options:['Sana','Mina','Chaeng','Dahyun'],
+        q: 'In the story of Stone soup, what did the villagers brought to add the soup?',
+        options:['Meat and Vegetables','Fruits and Vegetables','Candy and Pop corn'],
         answer:0
     },
     {
-        q: 'Is ur favorite music streaming service is Spotify?',
-        options:['True','False'],
+        q: ' In the story ugly duckling, ugly duckling turned into a ___? ',
+        options:['Beautiful Swan','Beautiful Duck','Beautiful Chicken'],
         answer:0
+    },
+    {
+        q: ' When the caterpillar is done forming inside the pupa, it will become ____ ?',
+        options:['Chicken','Horse','Butterfly'],
+        answer:2
     }
+    
     ]
     
 const english8 = [
     {
-        q: 'When is ur birthday?',
-        options:['January','February','May','June'],
-        answer:2
+        q: 'Why did Chonman keeps interrupting his father?',
+        options:['He feels like teasing his father',
+        'He already read the story and he just wants to have an answer to his curiosity.',
+        'He doesn’t want to listen to the stories from his father'],
+        answer:1
     },
     {
-        q: 'Who is ur bias?',
-        options:['Sana','Mina','Chaeng','Dahyun'],
+        q: 'Why did the princess does not answer Chonman’s Question?',
+        options:['She is not required to answer so she keeps quiet all the time.','She is under a spell of a witch',' She wants to play like a deaf.'],
+        answer:1
+    },
+    {
+        q: 'Did the princess enjoys the story telling of Chonman?',
+        options:['Yes, they both fell a sleep with a goodnight.','No, Princess fell a sleep before finishing the story telling.','No, Princess immediately get bored'],
         answer:0
     },
     {
-        q: 'Is ur favorite music streaming service is Spotify?',
-        options:['True','False'],
+        q: 'Why did Chonman wants to tell the story of “The Princess Who Can’t Answer Questions” to his father?',
+        options:[' Chonman wants to show off to his father','Chonman feels like he should tell it to his father because it might be a ghost.','Chonman feels that it is an interesting story to tell to'],
+        answer:2
+    },
+    {
+        q: 'If Princess visits you in your room, would you entertain her by reading some good stories for her?',
+        options:['Yes, I would love to',' No, I don’t want to','Yes, I also would like to be her friend'],
         answer:0
     }
     ]
     
 const english9 = [
     {
-        q: 'When is ur birthday?',
-        options:['January','February','May','June'],
+        q: 'The page at the back of the book that has words and their meanings is called ?',
+        options:['Index','Glossary','Spine'],
+        answer:1
+    },
+    {
+        q: 'The call number is found on this part of the book?',
+        options:['Spine','Title Page','Verso Page'],
+        answer:0
+    },
+    {
+        q: 'The date of publication can be found on this page?',
+        options:['Title page','Cover','Index'],
+        answer:1
+    },
+    {
+        q: 'This is a paper cover that some books have on the outside cover?',
+        options:['Book jacket',' Author','Illustrator'],
+        answer:0
+    },
+    {
+        q: 'the person who wrote the book is called?',
+        options:['Illustrator','Spine','Author'],
         answer:2
     },
     {
-        q: 'Who is ur bias?',
-        options:['Sana','Mina','Chaeng','Dahyun'],
+        q: 'Read the word this way _____ line by line.',
+        options:['left to right','right to left','all of the above'],
         answer:0
     },
     {
-        q: 'Is ur favorite music streaming service is Spotify?',
-        options:['True','False'],
+        q: 'Sentence ends in ____?',
+        options:['small letter','capital letter','period, exclamation, or question mark'],
+        answer:2
+    },
+    {
+        q: 'A capital letter is?',
+        options:['big','small','mark'],
         answer:0
+    },
+    {
+        q: 'A sentence that ask you will end with?',
+        options:['question mark (?)','exclamation mark (!)','period (.)'],
+        answer:0
+    },
+    {
+        q: 'A sentence with emotion will end with?',
+        options:['question mark (?)','exclamation mark (!)','period (.)'],
+        answer:1
     }
+    
+    
     ]
